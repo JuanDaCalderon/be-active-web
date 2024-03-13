7260618acbcb7c24c889a45e3362ae4a63ecd59d
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2024-03-13 15:34:33 -0500

    crear ejercicios commit v01

--- src/app/crear-ejercicio/crear-ejercicio.component.ts
+++ src/app/crear-ejercicio/crear-ejercicio.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CrearEjercicioModalComponent } from 'src/app/crear-ejercicio-modal/crear-ejercicio-modal.component'
+
 
 @Component({
   selector: 'app-crear-ejercicio',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './crear-ejercicio.component.scss'
 })
 export class CrearEjercicioComponent {
+  constructor(private dialog: MatDialog) {}
+
+  abrirModal(): void {
+    const dialogRef = this.dialog.open(CrearEjercicioModalComponent, {
+      width: '460px',
+
+    });
+  }
+
+  cerrarModal(): void {
+    this.dialog.closeAll();
+    }
 
 }
