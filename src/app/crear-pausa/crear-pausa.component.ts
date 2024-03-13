3c0d0b7bb2d10d7dfe63a224962803ed748aec67
lacortesz <la.cortesz1@uniandes.edu.co>
2024-03-13 15:44:54 -0500

    Crear pausa activa V1

--- src/app/crear-pausa/crear-pausa.component.ts
+++ src/app/crear-pausa/crear-pausa.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CrearPausaModalComponent } from 'src/app/crear-pausa-modal/crear-pausa-modal'
 
 @Component({
   selector: 'app-crear-pausa',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './crear-pausa.component.scss'
 })
 export class CrearPausaComponent {
+  constructor(private dialog: MatDialog) {}
+
+  abrirModal(): void {
+    const dialogRef = this.dialog.open(CrearPausaModalComponent, {
+      width: '460px',
+    });
+  }
+
+  cerrarModal(): void {
+    this.dialog.closeAll();
+    }
 
 }
